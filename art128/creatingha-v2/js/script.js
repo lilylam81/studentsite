e46b7f4ae5f478146a8a20316fa8bed934a62f04
lilylam81 <lilylam81@gmail.com>
2022-04-13 22:25:50 -1000

    updated draft #2

--- art128/creatingha-v2/js/script.js
+++ art128/creatingha-v2/js/script.js
@@ -25,6 +25,21 @@ document.addEventListener('DOMContentLoaded', function() {
         menunav.setAttribute('data-navstate', 'closed');
     };  
 
+    //WIDOW SLAYER - BY KCC NMA (https://codepen.io/kccnma/pen/LqOLPa)
+    var alltextblocks = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, a');
+    for ( var i=0; i<alltextblocks.length; i++ ) {
+        var mytb = alltextblocks[i].innerHTML;
+        var mytbs = alltextblocks[i].innerHTML.split( ' ' );
+        var lasttbs = mytb.slice(-1);
+        //console.log("length is " + mytbs.length + " last textblock is " + lasttbs);
+        if ((mytbs.length > 1) && (lasttbs != ">")) {
+            //console.log("widow slayed!");
+            mytbs[mytbs.length-2] += "&nbsp;" + mytbs[mytbs.length-1];
+            mytbs.pop();
+            alltextblocks[i].innerHTML = mytbs.join( ' ' );
+        }
+    };
+
 });
 
 
